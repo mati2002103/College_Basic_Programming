dba2562f226cfa129048541da61b95d539adc63d
Mati <mateusz.wisniewski.pl@gmail.com>
2023-05-28 13:46:09 +0200

    Added initials task

--- Homeworks/8.Strings/Initials.cpp
+++ Homeworks/8.Strings/Initials.cpp
@@ -4,21 +4,33 @@
 std::string initials(const std::string &fullName)
 {
 	std::string initials;
-	initials += fullName[0];
-	for(int i=1;i<=fullName.length();i++)
+	if(fullName.empty()){
+		return initials;
+
+	}
+	if (isalpha(fullName[0])) 
 	{
-		
-		if (isalpha(fullName[i]) && isspace(fullName[i-1]))
+		initials += fullName[0];
+	}
+	for (int i = 1; i <= fullName.length(); i++)
+	{
+
+		if (isalpha(fullName[i]) && isspace(fullName[i - 1]))
 		{
 			initials += fullName[i];
 		}
-		
+
 	}
 
 	return initials;
 }
-int main() 
+int main()
 {
-std::cout << initials("John Fitzgerald Kennedy") << std::endl;
-std::cout << initials(std::string("andy warhol")) << std::endl;
- }
+	std::string result = initials("");
+	std::string result1 = initials("John Fitzgerald Kennedy \n");
+	std::string result2 = initials(std::string("andy warhol"));
+	std::cout <<(int)result[0] << std::endl;
+	std::cout <<result1 << std::endl;
+	std::cout <<result2 << std::endl;
+}
+
