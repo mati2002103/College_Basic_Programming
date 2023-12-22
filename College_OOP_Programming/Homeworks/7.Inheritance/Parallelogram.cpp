3f0b3c4bf85bbbb425fdb5000482a76c1c6dbd8b
Mati <mateusz.wisniewski.pl@gmail.com>
2023-12-22 23:13:17 +0100

    aSd

--- College_OOP_Programming/Homeworks/7.Inheritance/Parallelogram.cpp
+++ College_OOP_Programming/Homeworks/7.Inheritance/Parallelogram.cpp
@@ -2,14 +2,27 @@
 
 class Parallelogram{
     private:
-    int sideA,sideB,height;
-    
-
+    double sideA,sideB,height;
     public:
+        Parallelogram(double sideA,double sideB,double height);
+        double area() const;
+        double perimeter() const;
+
 
 };
+Parallelogram::Parallelogram(double sideA,double sideB,double height) : sideA(sideA),sideB(sideB),height(height){};
+double Parallelogram::area() const{
+    return (double)sideA/height;
+}
+double Parallelogram::perimeter() const{
+    return double(2*sideB + 2*sideA);
+};
+class Rectangle : public Parallelogram{
+    private:
+    public:
+    Rectangle(double sideA,double sideB) : Parallelogram(sideA,sideB){};
 
-
+};
 
 
 
@@ -17,9 +30,11 @@ class Parallelogram{
 int main() {
 Parallelogram *rect = new Rectangle(4, 5);
 std::cout << rect->area() << " " << rect->perimeter() << std::endl;
+/*
 delete rectangle;
 Rectangle *square = new Square(6);
 std::cout << square->area() << " " << square->perimeter() << std::endl;
 delete square;
+*/
 return 0;
 }
